53b634a4b3ace883a50062d043a41eb45223a2b5
Luis Pujols <luispujols@hotmail.com>
2017-03-26 03:42:38 -0400

    ADD 'Created the Super Agent problem project'

---
+++ Codeforces/A. Super Agent/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char matrix[4][4];
+    for(int i=0; i<3; i++){
+        for(int q=0; q<3; q++){
+            char temp;
+            cin >> temp;
+            matrix[i][q] = temp;
+        }
+    }
+    for(int i=0; i<3; i++){
+        if(matrix[0][i] == 'X' && matrix[2][2-i] != 'X'){
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    if(matrix[1][0] == 'X' && matrix[1][2] != 'X'){
+        cout << "NO" << endl;
+    }
+    cout << "YES" << endl;
+    return 0;
+}
