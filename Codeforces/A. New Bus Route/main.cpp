a0720257616b389e46dfbd5efff0f38f8ca5a53a
Luis Pujols <luispujols@hotmail.com>
2017-03-27 18:26:02 -0400

    UPDATE 'Completed the problem'

--- Codeforces/A. New Bus Route/main.cpp
+++ Codeforces/A. New Bus Route/main.cpp
@@ -8,20 +8,20 @@ int main()
 {
     int n;
     cin >> n;
-    vector<int> cities;
+    vector<long long> cities;
     for(int i=0; i<n; i++){
-        int temp;
+        long long temp;
         cin >> temp;
         cities.push_back(temp);
     }
     sort(cities.begin(), cities.end());
-    int minimunDist = 1e9;
+    long long minimunDist = 10000000007;
     for(int i=0; i<n-1; i++){
-        minimunDist = min(abs(cities[i+1] - cities[i]), minimunDist);
+        minimunDist = min(abs(cities[i] - cities[i+1]), minimunDist);
     }
     int cnt = 0;
     for(int i=0; i<n-1; i++){
-        if(abs(cities[i+1] - cities[i] == minimunDist))
+        if(abs(cities[i] - cities[i+1]) == minimunDist)
             cnt++;
     }
     cout << minimunDist << " " << cnt;
