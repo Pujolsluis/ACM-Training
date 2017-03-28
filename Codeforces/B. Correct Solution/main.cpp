da6c41a31f3b60c782304d66513e6847fd3591c7
Luis Pujols <luispujols@hotmail.com>
2017-03-28 01:03:01 -0400

    REFACTOR 'Fixed some cases'

--- Codeforces/B. Correct Solution/main.cpp
+++ Codeforces/B. Correct Solution/main.cpp
@@ -9,11 +9,10 @@ int main()
     for(int i=0; i<s1.length()-1; i++){
         if(s1[i] > s1[i+1] && s1[i+1] != '0' || (s1[i] == '0' && i == 0)){
             cout << "WRONG_ANSWER" << endl;
-            cout << i << endl;
             return 0;
         }
     }
-    if(s1 < s)
+    if(s1 <= s && s1.length() == s.length())
         cout << "OK" << endl;
     else
         cout << "WRONG_ANSWER" << endl;
