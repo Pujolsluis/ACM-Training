364cdc2ee27bfd4133104950c8f3b1f76c5a3c81
Luis Pujols <luispujols@hotmail.com>
2017-04-07 22:45:26 -0400

    ADD 'Created the Army Problem'

--- Codeforces/B. Luxurious Houses/main.cpp
+++ Codeforces/B. Luxurious Houses/main.cpp
@@ -5,6 +5,13 @@ using namespace std;
 int main()
 {
     int n;
+    cin >> n;
+    vector<int> housesQ;
+    for(int i=0; i<n; i++){
+        int temp;
+        cin >> temp;
+        housesQ.push_back(temp);
+    }
 
     return 0;
 }
