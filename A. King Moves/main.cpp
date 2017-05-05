d8fc9817213b2f0518959aa3a4df5ea87fadace5
Luis Pujols <luispujols@hotmail.com>
2017-05-05 11:17:46 -0400

    Completed the King Moves, problem'

--- A. King Moves/main.cpp
+++ A. King Moves/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string s;
+    cin >> s;
+    char c = s[1];
+    char d = s[0];
+    if(c == '1' && d == 'a' || c == '1' && d == 'h' || c == '8' && d == 'a' || c == '8' && d == 'h'){
+        cout << 3 << endl;
+        return 0;
+    }else if(d == 'h' || d == 'a' || c == '1' || c == '8'){
+        cout << 5 << endl;
+        return 0;
+    }
+    cout << 8 << endl;
     return 0;
 }
