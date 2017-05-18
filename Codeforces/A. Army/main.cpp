e85000d2d521f9a515c6e4d0079f0319a71a6921
Luis Pujols <luispujols@hotmail.com>
2017-05-17 23:41:15 -0400

    UPDATE 'Organized the folder structure of the repository'

--- Codeforces/A. Army/main.cpp
+++ Codeforces/A. Army/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int N;
+    cin >> N;
+    vector<int> rankYears;
+    for(int i=0; i<N-1; i++){
+        int temp;
+        cin >> temp;
+        rankYears.push_back(temp);
+    }
+    int A,B;
+    cin >> A >> B;
+    A--;
+    B--;
+    int sol = 0;
+    for(int i=A; i<B; i++){
+        sol += rankYears[i];
+    }
+    cout << sol << endl;
     return 0;
 }
