619ff53db713fd488364bedd527a07f96a86831f
Luis Pujols <luispujols@hotmail.com>
2017-05-17 23:31:26 -0400

    ADD 'Created the Army problem project'

---
+++ A. Army/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> rankYears;
+    for(int i=0; i<N; i++){
+        int temp;
+        cin >> temp;
+        rankYears.push_back(temp);
+    }
+    int A,B;
+    cin >> A >> B;
+    A--;
+    B--;
+    int sol = 0;
+    for(int i=A; i<B; i++){
+        sol += rankYears[i];
+    }
+    cout << sol << endl;
+    return 0;
+}
