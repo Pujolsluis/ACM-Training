ca9331c15bfc4c85e0ac3b8024eea949c410dd78
Luis Pujols <luispujols@hotmail.com>
2017-05-18 23:45:00 -0400

    UPDATE 'Completed the Lucky Ticket Problem'

--- Codeforces/A. Lucky Ticket/main.cpp
+++ Codeforces/A. Lucky Ticket/main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+    int mid = N/2;
+    int cnt1 = 0, cnt2 = 0;
+    for(int i=0; i<S.length(); i++){
+        if(S[i] != '4' && S[i] != '7'){
+            cout << "NO" << endl;
+            return 0;
+        }
+        if(i<mid){
+            cnt1 += S[i] - '0';
+        }else{
+            cnt2 += S[i] - '0';
+        }
+    }
+    if(cnt1 == cnt2){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
     return 0;
 }
