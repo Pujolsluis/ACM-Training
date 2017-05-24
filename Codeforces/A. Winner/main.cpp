95f3063ed500c09397562487a999a75f11722433
Luis Pujols <luispujols@hotmail.com>
2017-05-24 00:02:12 -0400

    UPDATE 'Fixed the code'

--- Codeforces/A. Winner/main.cpp
+++ Codeforces/A. Winner/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <vector>
 using namespace std;
 
 int main()
@@ -8,8 +9,19 @@ int main()
     cin >> N;
     int best = 0;
     string bestPlayer;
+    map<string,int> gameScore;
+    vector<vector <string> > winners(1e6+7);
     for(int i=0; i<N; i++){
-
+        int tempScore;
+        string tempPlayer;
+        cin >> tempPlayer >> tempScore;
+        gameScore[tempPlayer]+= tempScore;
+        if(gameScore[tempPlayer] > best){
+            best = gameScore[tempPlayer];
+            bestPlayer = tempPlayer;
+            winners[gameScore].push
+        }
     }
+    cout << bestPlayer << endl;
     return 0;
 }
