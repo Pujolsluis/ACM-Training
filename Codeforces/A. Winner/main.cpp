ad75b5c4e23bd973dc5263b3ba26aedf419b20b9
Luis Pujols <luispujols@hotmail.com>
2017-05-23 23:47:52 -0400

    ADD 'Created the Winner Problem project'

---
+++ Codeforces/A. Winner/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int best = 0;
+    string bestPlayer;
+    for(int i=0; i<N; i++){
+
+    }
+    return 0;
+}
