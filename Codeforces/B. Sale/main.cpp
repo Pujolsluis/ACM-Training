e1f3d1ae01d4db483628226cf37575d5870a96ad
Luis Pujols <luispujols@hotmail.com>
2017-05-23 23:41:10 -0400

    UPDATE 'Completed the Sale problem'

--- Codeforces/B. Sale/main.cpp
+++ Codeforces/B. Sale/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int N,M;
+    cin >> N >> M;
+    vector<int> TvOnSale;
+    for(int i=0; i<N; i++){
+        int temp;
+        cin >> temp;
+        TvOnSale.push_back(temp);
+    }
+    sort(TvOnSale.begin(), TvOnSale.end());
+    int sol = 0;
+    for(int i=0; i<M; i++){
+        if(TvOnSale[i] < 0) sol += abs(TvOnSale[i]);
+    }
+    cout << sol << endl;
     return 0;
 }
