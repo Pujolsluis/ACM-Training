8821018dadec2af324e781e0ba15ab023f2eee5a
Luis Pujols Ogando <luispujols@hotmail.com>
2017-05-27 11:17:10 -0400

    ADD 'Created the tower of Hanoi Problem'

--- HackerEarth/Let's Begin!/main.cpp
+++ HackerEarth/Let's Begin!/main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+
+
+int go(int n){
+    if(n < 1) return 0;
+    int sol = 0;
+    if(n == '2' && n == '3' && n == '5' && n == '7'){
+        sol = 1 + go(n-1);
+    }
+    return sol;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int T;
+    cin >> T;
+    for(int i=0; i<T; i++){
+        int temp;
+        cin >> temp;
+        cout << go(temp) << endl;
+
+    }
     return 0;
 }
