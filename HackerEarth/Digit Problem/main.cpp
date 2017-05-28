d3ebac0c83f4b2e2dcd05e30ac4c0bd3957b3efa
Luis Pujols <luispujols@hotmail.com>
2017-05-28 00:19:24 -0400

    ADD 'Created and completed the Great Kean problem'

--- HackerEarth/Digit Problem/main.cpp
+++ HackerEarth/Digit Problem/main.cpp
@@ -4,17 +4,6 @@ using namespace std;
 
 int main()
 {
-    string X;
-    long long K;
-    cin >> X >> K;
-    long long index = 0;
-    while(K>0){
-        if(X[index] != '9'){
-            X[index] = '9';
-            K--;
-        }
-        index++;
-    }
-    cout << X << endl;
-    return 0;
+    char test = 'a' + 'b' - 'a';
+    cout << test << endl;
 }
