2cb75c2ad7b027181f6ffc2bb33ee7ce4e204525
Luis Pujols <luispujols@hotmail.com>
2017-09-13 23:02:30 -0400

    ADD ' Created and completed the problem Multiplos de 3 o 5'

--- Codeforces/F. Multiples de 3 o 5/main.cpp
+++ Codeforces/F. Multiples de 3 o 5/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
 using namespace std;
+
+bool isPrime(int n){
+    for(int i=2; i<=n/2; i++){
+        if(n%i == 0) return false;
+    }
+    return true;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    long long n, a, b;
+    cin >> n >> a >> b;
+    if(a == b) cout << n/a << endl;
+    else if(isPrime(a) && isPrime(b)){
+        cout << ((n/a) + (n/b)) - ((n/a)/b) << endl;
+    }else{
+        cout << (((n/a) + (n/b)) - ((n/a)/b))-1 << endl;
+    }
     return 0;
 }
